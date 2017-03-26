7248bd0c319798b1c2f445d4b60f02dd6de5698b
Akash <singh.aka@husky.neu.edu>
2017-03-26 01:34:24 -0400

    Push

--- DBLP/src/main/queryengine/app/utils/FilterUtils.java
+++ DBLP/src/main/queryengine/app/utils/FilterUtils.java
@@ -3,14 +3,9 @@ package queryengine.app.utils;
 import java.util.ArrayList;
 import java.util.List;
 
-import queryengine.interfaces.ISearch;
 import resources.person.IPerson;
 
 public class FilterUtils {
-	public static List<IPerson> filterResults(List<IFilter> searchCriteria, List<IPerson> searchResult) {
-		
-		return null;
-	}
 	
 	public static List<IPerson> filterByAuthorName(String authorName, List<IPerson> searchResult) {
 		List<IPerson> temp = new ArrayList<IPerson>();
