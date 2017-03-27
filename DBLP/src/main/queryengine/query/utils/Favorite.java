4fd247f51ba3887cc44962a93f72f97fff51fcea
Akash <singh.aka@husky.neu.edu>
2017-03-27 17:26:16 -0400

    Favorites Interface

--- DBLP/src/main/queryengine/query/utils/Favorite.java
+++ DBLP/src/main/queryengine/query/utils/Favorite.java
@@ -6,13 +6,19 @@ import queryengine.interfaces.IFavorite;
 
 public class Favorite implements IFavorite{
 
+	private String username; // This is username of the signed in user who adds favorites
 	private List<String> authorNames;
 	
-	public Favorite(List<String> authorNames) {
+	public Favorite(String username, List<String> authorNames) {
+		this.username = username;
 		this.authorNames = authorNames;
 	}
 	
 	public List<String> getAuthorNames() {
 		return this.authorNames;
+	}
+	
+	public String getUser() {
+		return this.username;
 	}
 }
