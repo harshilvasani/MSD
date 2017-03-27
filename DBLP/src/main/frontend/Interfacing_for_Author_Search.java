f24db8dab43afa1ecf8e0294831b6af79e0f02f3
Vineet Trivedi <trivedi.v@husky.neu.edu>
2017-03-27 13:24:41 -0400

    implemented filters and search page

--- DBLP/src/main/frontend/Interfacing_for_Author_Search.java
+++ DBLP/src/main/frontend/Interfacing_for_Author_Search.java
@@ -3,10 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import queryengine.app.App;
+import queryengine.interfaces.IFilter;
 import queryengine.interfaces.IQuery;
 import queryengine.interfaces.ISearch;
 import queryengine.query.utils.AbsenceFromCommittees;
+import queryengine.query.utils.AuthorName;
 import queryengine.query.utils.Conferences;
+import queryengine.query.utils.MinPublications;
 import queryengine.query.utils.Title;
 import queryengine.query.utils.Year;
 import resources.person.IPerson;
@@ -18,8 +21,7 @@ public class Interfacing_for_Author_Search {
 	private String journalName;
 	private String absenceYears;
 	private String authorNameContains;
-	private String minimumPublications;
-	private String favoriteAuthor;
+	private String minimumPublication;
 	private List<IPerson> result;
 	
 	public Interfacing_for_Author_Search(String titleText, String searchYear,
@@ -30,18 +32,36 @@ public class Interfacing_for_Author_Search {
 		this.absenceYears = absenceYears;
 	}
 	
+	public void setFilters(String authorNameContains, String minimumPublications){
+		this.authorNameContains = authorNameContains;
+		this.minimumPublication = minimumPublication;
+	}
+
 	public List<IPerson> sendSearchCriteriaToApp(){
 		App app = new App();
 		List<ISearch> searchCriteria = new ArrayList<ISearch>();
 		if (Validator.isNonEmptyString(this.titleText)) 
 			searchCriteria.add(new Title(this.titleText));
-		if (Validator.isValidYear(this.searchYear)) 	
+		if (Validator.isValidN(this.searchYear)) 	
 			searchCriteria.add(new Year(this.searchYear));
 		if (Validator.isNonEmptyString(this.journalName)) 	
 			searchCriteria.add(new Conferences(this.journalName));
 		if (Validator.isValidN(this.absenceYears))
-			searchCriteria.add(new AbsenceFromCommittees(this.absenceYears));
+			searchCriteria.add(new AbsenceFromCommittees(absenceYears));
 		this.result = app.search(searchCriteria);
+		return this.result;
+	}
+	
+	public List<IPerson> sendFiltersToApp(){
+		App app = new App();
+		List<IFilter> filterCriteria = new ArrayList<IFilter>();
+		
+		if (Validator.isNonEmptyString(this.authorNameContains)) 
+			filterCriteria.add(new AuthorName(this.authorNameContains));
+		if (Validator.isValidN(this.minimumPublication)) 	
+			filterCriteria.add(new MinPublications(this.minimumPublication));
+		
+		this.result = app.filter(filterCriteria, result);
 		return this.result;
 	}
 }
--- DBLP/src/main/frontend/Search_Page.java
+++ DBLP/src/main/frontend/Search_Page.java
@@ -4,19 +4,31 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
-import backend.BackEnd;
 import resources.person.Author;
 import resources.person.IPerson;
 
 import javax.swing.JFormattedTextField;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButton;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JPanel;
+import javax.swing.JCheckBox;
 
 public class Search_Page {
 
@@ -28,20 +40,21 @@ public class Search_Page {
 	JButton btnCoAuthorSearch;
 	JButton btnSimilarAuthorSearch;
 	JLabel lblAuthorSearch;
-	JFormattedTextField ftfSearchCriteria;
-	JFormattedTextField formattedTextField;
-	JFormattedTextField formattedTextField_1;
-	JFormattedTextField formattedTextField_2;
+	JFormattedTextField ftfPublicationTitle;
+	JFormattedTextField ftfJournalName;
+	JFormattedTextField ftfMinimumYear;
+	JFormattedTextField ftfAbsence;
+	JLabel lblDisplayingResults;
+	JLabel lblPublicationTitle;
+	JLabel lblJournalName;
+	JLabel lblMinimumYear;
+	JLabel lblAbsenceFromCommittee;
 	JButton btnSearch;
-	JTextField textField;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		BackEnd b = new BackEnd();
-		b.loadData(0);
-		System.out.println("Data LoADED");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -65,14 +78,13 @@ public class Search_Page {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
-	//	R1=false; R2=false; R3=false; R4=false;
+
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(255, 255, 255));
 		frame.setBounds(0, 0, 1350, 725);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		btnLogout = new JButton("Logout");
 		btnLogout.setBackground(new Color(255, 255, 255));
 		btnLogout.addActionListener(new ActionListener() {
@@ -86,7 +98,7 @@ public class Search_Page {
 		btnLogout.setForeground(new Color(0, 255, 0));
 		btnLogout.setBounds(1075, 10, 200, 25);
 		frame.getContentPane().add(btnLogout);
-		
+
 		btnProfile = new JButton("Profile");
 		btnProfile.setBackground(new Color(255, 255, 255));
 		btnProfile.addActionListener(new ActionListener() {
@@ -99,7 +111,7 @@ public class Search_Page {
 		btnProfile.setForeground(new Color(0, 255, 0));
 		btnProfile.setBounds(875, 10, 200, 25);
 		frame.getContentPane().add(btnProfile);
-		
+
 		btnFavorites = new JButton("Favorites List");
 		btnFavorites.setBackground(new Color(255, 255, 255));
 		btnFavorites.addActionListener(new ActionListener() {
@@ -109,7 +121,7 @@ public class Search_Page {
 		btnFavorites.setForeground(new Color(0, 255, 0));
 		btnFavorites.setBounds(675, 10, 200, 25);
 		frame.getContentPane().add(btnFavorites);
-		
+
 		btnAuthorSearch = new JButton("Author Search");
 		btnAuthorSearch.setBackground(new Color(255, 255, 255));
 		btnAuthorSearch.addActionListener(new ActionListener() {
@@ -119,7 +131,7 @@ public class Search_Page {
 		btnAuthorSearch.setForeground(new Color(0, 255, 0));
 		btnAuthorSearch.setBounds(475, 10, 200, 25);
 		frame.getContentPane().add(btnAuthorSearch);
-		
+
 		btnCoAuthorSearch = new JButton("Co-Author Search");
 		btnCoAuthorSearch.setBackground(new Color(255, 255, 255));
 		btnCoAuthorSearch.addActionListener(new ActionListener() {
@@ -129,7 +141,7 @@ public class Search_Page {
 		btnCoAuthorSearch.setForeground(new Color(0, 255, 0));
 		btnCoAuthorSearch.setBounds(275, 10, 200, 25);
 		frame.getContentPane().add(btnCoAuthorSearch);
-		
+
 		btnSimilarAuthorSearch = new JButton("Similar Author Search");
 		btnSimilarAuthorSearch.setBackground(new Color(255, 255, 255));
 		btnSimilarAuthorSearch.addActionListener(new ActionListener() {
@@ -146,99 +158,186 @@ public class Search_Page {
 		lblAuthorSearch.setForeground(new Color(0, 255, 0));
 		lblAuthorSearch.setBounds(150, 60, 200, 28);
 		frame.getContentPane().add(lblAuthorSearch);
-		
-		ftfSearchCriteria = new JFormattedTextField();
-		ftfSearchCriteria.setForeground(new Color(0, 255, 0));
-		ftfSearchCriteria.setText("");
-		ftfSearchCriteria.setBounds(150, 120, 200, 20);
-		frame.getContentPane().add(ftfSearchCriteria);
-		
-		formattedTextField = new JFormattedTextField();
-		formattedTextField.setText("");
-		formattedTextField.setForeground(Color.GREEN);
-		formattedTextField.setBounds(150, 151, 200, 20);
-		frame.getContentPane().add(formattedTextField);
-		
-		formattedTextField_1 = new JFormattedTextField();
-		formattedTextField_1.setText("");
-		formattedTextField_1.setForeground(Color.GREEN);
-		formattedTextField_1.setBounds(150, 182, 200, 20);
-		frame.getContentPane().add(formattedTextField_1);
-		
-		formattedTextField_2 = new JFormattedTextField();
-		formattedTextField_2.setText("");
-		formattedTextField_2.setForeground(Color.GREEN);
-		formattedTextField_2.setBounds(150, 213, 200, 20);
-		frame.getContentPane().add(formattedTextField_2);
-		
+
+		ftfPublicationTitle = new JFormattedTextField();
+		ftfPublicationTitle.setForeground(new Color(0, 255, 0));
+		ftfPublicationTitle.setText("");
+		ftfPublicationTitle.setBounds(150, 120, 200, 20);
+		frame.getContentPane().add(ftfPublicationTitle);
+
+		ftfJournalName = new JFormattedTextField();
+		ftfJournalName.setText("");
+		ftfJournalName.setForeground(Color.GREEN);
+		ftfJournalName.setBounds(150, 151, 200, 20);
+		frame.getContentPane().add(ftfJournalName);
+
+		ftfMinimumYear = new JFormattedTextField();
+		ftfMinimumYear.setText("");
+		ftfMinimumYear.setForeground(Color.GREEN);
+		ftfMinimumYear.setBounds(150, 182, 200, 20);
+		frame.getContentPane().add(ftfMinimumYear);
+
+		ftfAbsence = new JFormattedTextField();
+		ftfAbsence.setText("");
+		ftfAbsence.setForeground(Color.GREEN);
+		ftfAbsence.setBounds(150, 213, 200, 20);
+		frame.getContentPane().add(ftfAbsence);
+
 
 		btnSearch = new JButton("Search");
 		btnSearch.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				
-				String publicationTitle = ftfSearchCriteria.getText();
-				String journalName = formattedTextField.getText();
-				String minYear = formattedTextField_1.getText();
-				String absence = formattedTextField_2.getText();
-				
+
+				String publicationTitle = ftfPublicationTitle.getText();
+				String journalName = ftfJournalName.getText();
+				String minYear = ftfMinimumYear.getText();
+				String absence = ftfAbsence.getText();
+
 				Interfacing_for_Author_Search authorSearchComm = new Interfacing_for_Author_Search(
 						publicationTitle, journalName, minYear, absence);
-				
+
 				List<IPerson> result = authorSearchComm.sendSearchCriteriaToApp();
+
+		/*		List<IPerson> result = new ArrayList<IPerson>();
+				for(int i=1;i<=50;i++){
+					result.add(new Author("abc", i, "abc", "abc", "6", "article"));
+					result.add(new Author("abc", i, "abc", "abc", "7", "article"));
+				} */
 				
-				String display = "";
-				
-				for(int i=0; i<result.size(); i++){
-					IPerson person = result.get(i);
-					if(person instanceof Author){
-						Author a = (Author) person;
-						display += a.toString();
-						display += ";\n";
-					}
+				if(result != null && result.size() > 0){
+					displaySearchResults(result,authorSearchComm);
 				}
-				
-				textField.setText(display);
-				
 			}
 		});
 		btnSearch.setForeground(Color.GREEN);
 		btnSearch.setBackground(Color.WHITE);
 		btnSearch.setBounds(192, 244, 110, 25);
 		frame.getContentPane().add(btnSearch);
-		
-		
-		textField = new JTextField();
-		textField.setBounds(150, 376, 200, 144);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-	/*	
-		JButton button = new JButton("Search");
-		button.setForeground(Color.GREEN);
-		button.setBackground(Color.WHITE);
-		button.setBounds(24, 305, 110, 25);
-		frame.getContentPane().add(button);
-		*/
-		JLabel lblDisplayingResults = new JLabel("Displaying Results");
-		lblDisplayingResults.setBounds(151, 345, 86, 14);
-		frame.getContentPane().add(lblDisplayingResults);
-		
-		JLabel lblPublicationTitle = new JLabel("Publication Title");
+
+		lblPublicationTitle = new JLabel("Publication Title");
 		lblPublicationTitle.setBounds(24, 123, 116, 14);
 		frame.getContentPane().add(lblPublicationTitle);
-		
-		JLabel lblJournalName = new JLabel("Journal Name");
+
+		lblJournalName = new JLabel("Journal Name");
 		lblJournalName.setBounds(24, 154, 116, 14);
 		frame.getContentPane().add(lblJournalName);
-		
-		JLabel lblMinimumYear = new JLabel("Minimum Year");
+
+		lblMinimumYear = new JLabel("Minimum Year");
 		lblMinimumYear.setBounds(24, 185, 116, 14);
 		frame.getContentPane().add(lblMinimumYear);
-		
-		JLabel lblAbsenceFromCommittee = new JLabel("Absence from Committee");
+
+		lblAbsenceFromCommittee = new JLabel("Absence from Committee");
 		lblAbsenceFromCommittee.setBounds(24, 216, 116, 14);
 		frame.getContentPane().add(lblAbsenceFromCommittee);
+
+
+	}
+
+	public void displaySearchResults(List<IPerson> result, Interfacing_for_Author_Search authorSearchComm){
+
+		JLabel lblAuthorNameContains = new JLabel("Author Name Contains:");
+		lblAuthorNameContains.setBounds(192, 300, 150, 25);
+		frame.getContentPane().add(lblAuthorNameContains);
+
+		JFormattedTextField ftfAuthorNameContains = new JFormattedTextField();
+		ftfAuthorNameContains.setForeground(new Color(0, 255, 0));
+		ftfAuthorNameContains.setText("");
+		ftfAuthorNameContains.setBounds(352, 300, 200, 25);
+		frame.getContentPane().add(ftfAuthorNameContains);
+
+		JLabel lblMinimumPublication = new JLabel("Minimum Publication:");
+		lblMinimumPublication.setBounds(572, 300, 150, 25);
+		frame.getContentPane().add(lblMinimumPublication);
+
+		JFormattedTextField ftfMinimumPublication = new JFormattedTextField();
+		ftfMinimumPublication.setForeground(new Color(0, 255, 0));
+		ftfMinimumPublication.setText("");
+		ftfMinimumPublication.setBounds(732, 300, 200, 25);
+		frame.getContentPane().add(ftfMinimumPublication);
+
+		lblDisplayingResults = new JLabel("Displaying Results");
+		lblDisplayingResults.setBounds(150, 345, 200, 14);
+		frame.getContentPane().add(lblDisplayingResults);
+
+		JPanel authorDisplayArea = new JPanel();
+		authorDisplayArea.setLayout(new BoxLayout(authorDisplayArea, BoxLayout.Y_AXIS));
+
+		JScrollPane scroller = new JScrollPane(authorDisplayArea,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scroller.setBounds(150, 370, 1000, 300);
+		frame.getContentPane().add(scroller);
 		
-		
+		JButton btnFilter = new JButton("Apply Filters");
+		btnFilter.setBackground(new Color(255, 255, 255));
+		btnFilter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				authorSearchComm.setFilters(ftfAuthorNameContains.getText(),
+						ftfMinimumPublication.getText());
+
+				List<IPerson> filterSet = authorSearchComm.sendFiltersToApp();
+			/*	List<IPerson> filterSet = new ArrayList<IPerson>();
+				for(int i=1;i<=50;i++){
+					filterSet.add(new Author("abc", i, "abc", "abc", "8", "article"));
+					filterSet.add(new Author("abc", i, "abc", "abc", "9", "article"));
+				} */
+				
+				frame.getContentPane().remove(scroller);
+				if(result != null && result.size() > 0){
+					displaySearchResults(filterSet,authorSearchComm);
+				}
+
+			}
+		});
+		btnFilter.setForeground(new Color(0, 255, 0));
+		btnFilter.setBounds(952, 300, 200, 25);
+		frame.getContentPane().add(btnFilter);
+
+		DefaultTableModel model = new DefaultTableModel();
+		JTable searchResults = new JTable(model);
+		authorDisplayArea.add(searchResults);  
+
+		JButton btnaddFavoriteAuthor = new JButton("Add to Favorites");
+		btnaddFavoriteAuthor.setBackground(new Color(255, 255, 255));
+		btnaddFavoriteAuthor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int row = searchResults.getSelectedRow();
+				System.out.println(searchResults.getValueAt(row,0)+" "+
+						searchResults.getValueAt(row,1)+" "+searchResults.getValueAt(row,2)+" "+
+						searchResults.getValueAt(row,3)
+						);
+			}
+		});
+		btnaddFavoriteAuthor.setForeground(new Color(0, 255, 0));
+		btnaddFavoriteAuthor.setBounds(550, 345, 200, 15);
+		frame.getContentPane().add(btnaddFavoriteAuthor);
+
+		model.addColumn("Person Name"); 
+		model.addColumn("Publication Title");
+		model.addColumn("Publication Type");
+		model.addColumn("Publication Year");
+
+		model.addRow(new Object[]{"Person Name","Publication Title",
+				"Publication Type", "Publication Year"});
+
+		if(result != null && result.size() > 0){
+			for( int i = 0; i < result.size() ; i++ ){
+
+				IPerson person = result.get(i);
+
+				if( person instanceof Author ){
+
+					Author a = (Author) person;
+
+					model.addRow(new Object[]{a.getPersonName(),a.getPublicationTitle(),
+							a.getPublicationType(), a.getPublicationYear()});
+
+
+				}
+			}
+		}
+
+		frame.repaint();
+
 	}
 }
